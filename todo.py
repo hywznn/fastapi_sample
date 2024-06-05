5736b52eaf0ae0486f077d9024af4916ef59edde
hywznn <choihj0510@naver.com>
2024-06-05 21:02:03 +0900

    윽

--- todo.py
+++ todo.py
@@ -7,10 +7,19 @@ todo_list = []
 
 @todo_router.post("/todo")
 async def add_todo(todo: Todo) -> dict:
+    todo.id = len(todo_list)+1
     todo_list.append(todo)
     return {
         "msg" : "todo added successfully"
     }
+    
+@todo_router.delete("/todo/{todo_id}")
+async def delete_todo(todo_id: int = Path(..., title="the ID of the todo to delete")) -> dict:
+    for index, todo in enumerate(todo_list):
+        if todo.id ==todo_id:
+            del todo_list[index]
+            return {"msg" : f"Todo with ID {todo_id} deleted successfully"}
+        return {"msg": "Todo with supplied ID doesn't exist"}
 
 @todo_router.get("/todo")
 async def retrieve_todos() -> dict:
